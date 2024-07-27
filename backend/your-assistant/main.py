7ee9a32294571bd16f3ccf9cea228c138486a86f
Bikash <kcbiaksh@gmail.com>
2024-07-27 20:24:44 +1000

    Init: Initialised the backend by setting up the necessary files

---
+++ backend/your-assistant/main.py
@@ -0,0 +1,14 @@
+"""
+The backend of the Legal_AI_App. The backend is a project 
+named your-assistant.
+"""
+
+def run_server():
+    print("hello world")
+
+if __name__ == "__main__":
+    """In case you are invoking this via Python directly.
+    This is probably never actually used but it is here for completeness.
+    You'd execute this by running `python -m your-assistant.main`
+    """
+    run_server()
