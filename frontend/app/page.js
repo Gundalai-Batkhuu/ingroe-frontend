3b9db9469b5637815f749bad66699fa1961c3173
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-07-29 22:43:25 +1000

    #2 Added login and sign up pages. No interaction

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center justify-between p-24">
         <div className="flex flex-col items-center justify-center space-y-8">
             <h1 className="text-4xl font-bold">Welcome to the Legal AI App!</h1>
             <p className="text-lg text-center">
