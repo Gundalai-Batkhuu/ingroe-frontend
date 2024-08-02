07d19b47de39f57031bac0e03f8c6bd55ea1bb7c
Bikash <kcbiaksh@gmail.com>
2024-08-02 14:06:35 +1000

    update: added the functionality to increase the api calls if the results to retrieve is higher than 10

--- backend/app/external/search_func.py
+++ backend/app/external/search_func.py
@@ -1,8 +1,9 @@
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
-from typing import List
+from typing import List, Tuple, Dict
 from app.model.pydantic_model.data_model import SearchResult
+import math
 
 load_dotenv()
 
@@ -13,31 +14,78 @@ class SearchFunction:
     google_cse_id = os.getenv("GOOGLE_CSE_ID")
 
     @classmethod
-    async def _get_result_from_engine(cls, query: str) -> List[dict]:
+    async def _get_result_from_engine(cls, query: str, **kwargs) -> List[dict]:
         """Returns raw results based on a search query from the engine.
 
         Args:
-        Query string.
+        query: Query string.
 
         Returns:
-        A list containing a dictionary of search result properties
+        A list containing a dictionary of search result properties.
         """
         service = build("customsearch", "v1", developerKey=cls.google_api_key)
-        res = service.cse().list(q=query, cx=cls.google_cse_id).execute()
+        res = service.cse().list(q=query, cx=cls.google_cse_id, **kwargs).execute()
         return res["items"]
     
     @classmethod
-    async def get_result(cls, query: str) -> List[SearchResult]:
+    def _get_num_of_api_calls(cls, num_of_results: int) -> int:
+        """Determines the number of api calls to make based on the number of results
+        that users want to get from a search query. Single call can fetch only 10 results max.
+
+        Args:
+        num_of_results: Indicates the number of search results that a user want to get.
+
+        Returns:
+        The number of calls to make.
+        """
+        if num_of_results > 10:
+            calls_to_make = math.ceil(num_of_results / 10)
+            return calls_to_make
+        calls_to_make = 1
+        return calls_to_make
+    
+    @classmethod
+    async def _get_total_results(cls, query: str, num_of_results: int, api_calls: int) -> List[dict]:
+        """Based on the number of api calls to make and the total results an user want,
+        the results will be appended to return the total result.
+
+        Args:
+        num_of_results: Indicates the number of search results that a user want to get.
+        query: Query string.
+        api_calls: Number of api calls to make.
+
+        Returns:
+        A complete list containing a dictionary of search result properties.
+
+        """
+        start_item = 1
+        result_count = 10
+        items_to_return = []
+        while api_calls > 0:
+            results = await cls._get_result_from_engine(query, start=start_item, num=result_count)
+            items_to_return.extend(results)
+            api_calls -= 1
+            start_item += 10
+            remaining_results = num_of_results - start_item + 1
+            if 0 < remaining_results < 10:
+                result_count = remaining_results
+        return items_to_return        
+
+    
+    @classmethod
+    async def get_result(cls, query: str, num_of_results: int) -> List[SearchResult]:
         """Returns formatted results based on a search query. If exception or error occurs it
         returns an empty list.
 
         Args:
-        Query string.
+        query: Query string.
 
         Returns:
         A list containing a dictionary of search result properties
         """
-        results = await cls._get_result_from_engine(query)
+        num_of_api_calls = cls._get_num_of_api_calls(num_of_results)
+        results = await cls._get_total_results(query, num_of_results, num_of_api_calls)
+        print(f"length of search result is {len(results)}")
         formatted_results = []
         try:
             for result in results:
