a2daaf3c5fa4414559192dfd2ef9e7a29cd9557b
Bikash <kcbiaksh@gmail.com>
2024-08-05 11:14:41 +1000

    add: added the CreateDocument model for the document creation route

--- backend/app/routes/action.py
+++ backend/app/routes/action.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from typing import Dict
-from ..model.pydantic_model.data_model import SearchQuery
-from ..controller.doc_action.search import Search
+from app.model.pydantic_model.data_model import (SearchQuery, CreateDocument)
+from app.controller.doc_action.search import Search
+from app.controller.doc_action.create_document import Create
 
 router = APIRouter(
     prefix="/items",
@@ -13,8 +14,12 @@ router = APIRouter(
 def index() -> Dict[str,str]:
     return {"name": "hello"}
 
-@router.post("/search-document")
-async def search(query_object: SearchQuery):
-    results = await Search.search_documents(query_object)
-    # return query_object
+@router.post("/search-query")
+async def search(payload: SearchQuery):
+    results = await Search.search_documents(payload)
     return results
+
+@router.post("/create-document")
+async def create_document(payload: CreateDocument):
+    Create.create_document(payload)
+    return payload
