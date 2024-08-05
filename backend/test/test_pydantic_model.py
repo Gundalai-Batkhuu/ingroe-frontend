7158912c058862c0f01b15879295df29b993240f
Bikash <kcbiaksh@gmail.com>
2024-08-06 09:29:06 +1000

    update: imposed new rules in the data validation for search operation

--- backend/test/test_pydantic_model.py
+++ backend/test/test_pydantic_model.py
@@ -20,4 +20,20 @@ def test_before_after_date():
     """     
     with pytest.raises(ValueError) as excinfo:
         SearchQuery(query="hello", country_specific_search=True, country="Australia", before=2015, after=2010) 
-    assert "Cannot have before and after date set for a search query." in str(excinfo.value)    
+    assert "Cannot have before and after date set for a search query." in str(excinfo.value)  
+
+def test_mix_and_number_of_results():
+    """
+    Test if the results required is not 1 when mix result is preferred.
+    """     
+    with pytest.raises(ValueError) as excinfo:
+        SearchQuery(query="hello", country_specific_search=True, country="Australia", mix=True, results=1) 
+    assert "Cannot provide mix results for a single result." in str(excinfo.value)
+
+def test_mix_and_file_type():
+    """
+    Test if file type is provided when mix result is preferred.
+    """     
+    with pytest.raises(ValueError) as excinfo:
+        SearchQuery(query="hello", country_specific_search=True, country="Australia", mix=True, result=6) 
+    assert "Must provide a file type if mix result is required." in str(excinfo.value)           
