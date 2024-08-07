b4ac68019ee5c062e43fb11d5b139bb56a215109
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-08-08 00:25:43 +1000

    Update settings

--- frontend/app/signup/page.js
+++
@@ -1,9 +0,0 @@
-import SignupForm from '../components/signup-form'
-
-export default function SignupPage() {
-  return (
-    <main className="flex flex-col p-4 my-20">
-      <SignupForm />
-    </main>
-  )
-}
