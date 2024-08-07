a8db6541a4cfe8435de9c467132ac75b37be14e9
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-08-08 00:43:06 +1000

    Test deployment

--- frontend/app/chat/layout.js
+++ frontend/app/chat/layout.js
@@ -1,7 +1,7 @@
 
 export default async function ChatLayout({ children }) {
   return (
-    <div className="relative flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
+    <div className="flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
       {children}
     </div>
   )
