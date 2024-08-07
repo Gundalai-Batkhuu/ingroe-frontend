92139ae97acd25e5156301d8f124078dfd264955
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-08-08 00:25:43 +1000

    Update settings

--- frontend/app/login/page.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from '../components/login-form'
-
-export default function LoginPage() {
-  return (
-    <main className="flex flex-col p-4 my-20">
-      <LoginForm />
-    </main>
-  )
-}
