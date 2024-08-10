c92af200a368348aa4b104a9c3570f3cfab3bdef
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-08-10 22:15:42 +1000

    Add search results component

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,9 +4,23 @@ The backend of the Legal_AI_App.
 
 from fastapi import FastAPI
 from .routes import action
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 prefix = "/api/v1"
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router=action.router, prefix=prefix)
 
 @app.get("/")
--- frontend/app/components/providers.tsx
+++ frontend/app/components/providers.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 import { SidebarProvider } from '../lib/hooks/use-sidebar'
-import { TooltipProvider } from './ui/tooltip'
+import { TooltipProvider } from './tooltip'
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
