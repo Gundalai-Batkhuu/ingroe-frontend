6d1eee45f91af543e3fd76ed8143fc4fc8efc700
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-08-13 00:06:00 +1000

    Add Vercel's chat template

--- frontend/app/chat/layout.js
+++
@@ -1,8 +0,0 @@
-
-export default async function ChatLayout({ children }) {
-  return (
-    <div className="flex h-[calc(100vh_-_theme(spacing.16))] overflow-hidden">
-      {children}
-    </div>
-  )
-}
---
+++ frontend/app/components/sidebar-desktop.tsx
@@ -0,0 +1,20 @@
+import { SearchSidebar } from './search-sidebar'
+
+import { auth } from '@/auth'
+import { ChatHistory } from './chat-history'
+import {ChatSidebar} from "@/app/components/chat-sidebar";
+
+export async function SidebarDesktop() {
+  const session = await auth()
+
+  if (!session?.user?.id) {
+    return null
+  }
+
+  return (
+    <ChatSidebar className="peer absolute inset-y-0 z-30 hidden -translate-x-full border-r bg-muted duration-300 ease-in-out data-[state=open]:translate-x-0 lg:flex lg:w-[250px] xl:w-[300px]">
+      {/* @ts-ignore */}
+      <ChatHistory userId={session.user.id} />
+    </ChatSidebar>
+  )
+}
