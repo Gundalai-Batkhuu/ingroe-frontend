3ff7b46e91f1119cc14b502dadddaa8c25bf1cdd
Bikash <kcbiaksh@gmail.com>
2024-08-17 17:52:44 +1000

    add: added the document nodel and the table plus created a document record on document creation

--- backend/app/model/db/__init__.py
+++ backend/app/model/db/__init__.py
@@ -1,3 +1,4 @@
 from app.model.db.user_model import User
+from app.model.db.document_model import Document
 
-__all__ = ["User"]
+__all__ = ["User", "Document"]
--- backend/app/model/db/user_model.py
+++ backend/app/model/db/user_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
-from app.database import Base, get_db
+from app.database import Base
 from datetime import datetime, timezone
+from sqlalchemy.orm import relationship
 
 class User(Base):
     """Model for the user table with id and created_at column populated with automatic values.
@@ -12,5 +13,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-    user_id = Column(String, index=True, nullable=False)
+    user_id = Column(String, index=True, unique=True, nullable=False)
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+
+    document = relationship("Document", back_populates="user")
