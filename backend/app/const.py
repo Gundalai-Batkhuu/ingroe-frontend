bef2d51b278908ea84e33b50b05e7ca7c41d352a
Bikash <kcbiaksh@gmail.com>
2024-08-31 16:17:17 +1000

    update: included bedrock models in the collection of llm models

--- backend/app/const.py
+++ backend/app/const.py
@@ -22,6 +22,8 @@ class ModelDetails:
     """   
     BEDROCK_CLAUDE_HAIKU = "anthropic.claude-3-haiku-20240307-v1:0" 
     BEDROCK_CLAUDE_SONNET = "anthropic.claude-3-sonnet-20240229-v1:0"  
+    GROQ_LLAMA_70B = "llama3-70b-8192"
+    OPENAI_GPT_TURBO = "gpt-3.5-turbo-0125"
 
 class FileType:
     """Contains the file types.
