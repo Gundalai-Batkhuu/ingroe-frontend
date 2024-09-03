901fa278375d6919ba86086eb4422affe31b093c
Gundalai Batkhuu <gdbt0623@gmail.com>
2024-09-03 16:58:07 +1000

    Fix

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,21 +3,16 @@ from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+database_url = os.getenv("DATABASE_URL")
+use_cloud_db = os.getenv("USE_CLOUD_DB")
+if use_cloud_db:
+    engine = create_engine(database_url, connect_args={
+    "sslmode": "require"})
+else:
+    engine = create_engine(database_url)
 
-# Construct the database URL
-database_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(database_url, connect_args={
-    "sslmode": "require",
-})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base: DeclarativeMeta = declarative_base()
 
