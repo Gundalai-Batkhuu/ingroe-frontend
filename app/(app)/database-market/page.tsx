5555260d29cca76b7a9ae45c81880f5ee3806276
Gundalai Batkhuu <gdbt0623@gmail.com>
2025-02-12 12:22:57 +1100

    Remove unused routes

--- app/(app)/database-market/page.tsx
+++
@@ -1,5 +0,0 @@
-import DatabaseCollectionPageContent from '@/features/public-database-collection/components/database-collection-page-content';
-
-export default async function MarketPage() {
-	return <DatabaseCollectionPageContent />;
-}
